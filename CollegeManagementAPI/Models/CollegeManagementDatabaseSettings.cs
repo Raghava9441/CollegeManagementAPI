ccbfb8956fd90ad7ebd4e239faa3f864eed73706
Raghavendra Reddy <raghavendra.reddy@htown-tech.com>
2024-05-11 20:56:27 +0530

    Add project files.

---
+++ CollegeManagementAPI/Models/CollegeManagementDatabaseSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace CollegeManagementAPI.Models
+{
+    public class CollegeManagementDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string OrganizationCollectionName { get; set; } = null!;
+        public string TeacherCollectionName { get; set; } = null!;
+        public string StudentsCollectionName { get; set; } = null!;
+        public string ParentCollectionName { get; set; } = null!;
+
+    }
+}
